66f42f5b68df36ccd54486d02edc47d5797c51c3
leto-design <blackshift.aze@gmail.com>
2026-01-15 19:08:16 +0700

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -39,11 +39,6 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.Explorer(),
   ],
-  right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
