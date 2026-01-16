2def4ff04f0509567bda672541c38a62740e6d67
leto-design <blackshift.aze@gmail.com>
2026-01-16 17:08:43 +0700

    header font

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Handjet",
+        header: "Press Start 2P",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
